73f3b8f354b9611b99a3d0e9bdf37125396b3dac
DmytroDT <Dohgca@gmail.com>
2020-11-08 16:49:29 +0200

    Formatted code.

--- Lab1/src/FibonacchiNumberline.java
+++ Lab1/src/FibonacchiNumberline.java
@@ -1,128 +1,102 @@
 import java.util.ArrayList;
 
 import javax.naming.directory.InvalidAttributeIdentifierException;
+
 /**
  * 
- * @author dmytro
- * A class that holds numerous variables with properties of numerical series. 
- * Main way to work with it is through it's methods. 
+ * @author dmytro A class that holds numerous variables with properties of
+ *         numerical series. Main way to work with it is through it's methods.
  */
 
-public class FibonacchiNumberline 
-{
-	private int lowerBoarder,upperBoarder, oddSum,evenSum,Fibborder,oddCount=1,evenCount=1;
-	private long var1,var2,intermeiateResult; 
+public class FibonacchiNumberline {
+	private int lowerBoarder, upperBoarder, oddSum, evenSum, Fibborder, oddCount = 1, evenCount = 1;
+	private long var1, var2, intermeiateResult;
 	private ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
 	private ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
 	private ArrayList<Long> fibNumbers = new ArrayList<Long>();
-	
-	
-	public FibonacchiNumberline(int lowerBoarder, int upperBoarder,int Fibonborder)
-	{
+
+	/**
+	 * 
+	 * @param lowerBoarder - lower border of numerical series
+	 * @param upperBoarder - upper border of numerical series
+	 * @param Fibonborder  - amout of fibonacchi numbers that will be created
+	 */
+	public FibonacchiNumberline(int lowerBoarder, int upperBoarder, int Fibonborder) {
 		this.lowerBoarder = lowerBoarder;
 		this.upperBoarder = upperBoarder;
-		this.Fibborder=Fibonborder;
-		
+		this.Fibborder = Fibonborder;
+
 		SelectNumbers();
 		NumbersSum();
 		FibonacchiSeries();
 	}
-	
-	
-	
-	
-	private void SelectNumbers()
-	{
-		
-		for(int i= lowerBoarder; i <= upperBoarder; i++)
-		{
-			if(i%2==0)
-			{
+
+	private void SelectNumbers() {
+
+		for (int i = lowerBoarder; i <= upperBoarder; i++) {
+			if (i % 2 == 0) {
 				evenNumbers.add(i);
 
-			}
-			else 
-			{
+			} else {
 				oddNumbers.add(i);
 			}
 		}
 
 	}
-	
-	
-	private	void NumbersSum()
-	{
-	
-		for(int i= lowerBoarder; i < upperBoarder; i++)
-		{
-			if(i%2==0)
-			{
-				evenSum+=i;
-			}
-			else 
-			{
-				oddSum+=i;
-			}
-		
-		}
-	}
-	
-	private void FibonacchiSeries()
-	{
-		
-		var1=oddNumbers.get(oddNumbers.size()-1);
-		var2=evenNumbers.get(evenNumbers.size()-1);
-		fibNumbers.add(var1);
-		fibNumbers.add(var2);
-		
-		
-		for (int i=0; i< Fibborder;i++)
-		{
-			intermeiateResult=var1+var2;
-			var1=var2;
-			var2=intermeiateResult;
-		
-			fibNumbers.add(intermeiateResult);
-			
-			if(intermeiateResult%2==0) 
-			{
-				evenCount++;
-			}
-			else
-			{
-			oddCount++;
-			}
-		}
-	}
-	
-	
-	
-	public void DisplayNumbers()
-	{
-			System.out.println("Even numbers:");
-			for (int i=0;i<evenNumbers.size();i++)
-			{
-				System.out.println(evenNumbers.get(i));
-			}
-			System.out.println("Odd numbers:");
-			for (int i=oddNumbers.size()-1;i>=0;i--)
-			{
-				System.out.println(oddNumbers.get(i));
-			}
-			
-			System.out.println("Odd numbers sum :"+oddSum+" even numbers sum:"+evenSum);
-			System.out.println("Fibonacci series:");
-			for(int i=0;i<fibNumbers.size();i++)
-			{
-				System.out.println(fibNumbers.get(i));
+
+	private void NumbersSum() {
+
+		for (int i = lowerBoarder; i < upperBoarder; i++) {
+			if (i % 2 == 0) {
+				evenSum += i;
+			} else {
+				oddSum += i;
 			}
 
-			System.out.println("Even number percentage is:"+(evenCount*100/fibNumbers.size())+"%.");
-			System.out.println("Odd number percentage is:"+(oddCount*100/fibNumbers.size())+"%.");
-		
-		
+		}
 	}
-			
-	
-	
+
+	private void FibonacchiSeries() {
+
+		var1 = oddNumbers.get(oddNumbers.size() - 1);
+		var2 = evenNumbers.get(evenNumbers.size() - 1);
+		fibNumbers.add(var1);
+		fibNumbers.add(var2);
+
+		for (int i = 0; i < Fibborder; i++) {
+			intermeiateResult = var1 + var2;
+			var1 = var2;
+			var2 = intermeiateResult;
+
+			fibNumbers.add(intermeiateResult);
+
+			if (intermeiateResult % 2 == 0) {
+				evenCount++;
+			} else {
+				oddCount++;
+			}
+		}
+	}
+
+	public void DisplayNumbers() {
+		System.out.println("Even numbers:");
+		for (int i = 0; i < evenNumbers.size(); i++) {
+			System.out.println(evenNumbers.get(i));
+		}
+		System.out.println("Odd numbers:");
+		for (int i = oddNumbers.size() - 1; i >= 0; i--) {
+			System.out.println(oddNumbers.get(i));
+		}
+
+		System.out.println("Odd numbers sum :" + oddSum + " even numbers sum:" + evenSum);
+		System.out.println("Fibonacci series:");
+		for (int i = 0; i < fibNumbers.size(); i++) {
+			System.out.println(fibNumbers.get(i));
+		}
+
+		System.out.println("Even number percentage is:" + (evenCount * 100 / fibNumbers.size()) + "%.");
+		System.out.println("Odd number percentage is:" + (oddCount * 100 / fibNumbers.size()) + "%.");
+
+	}
+
 }
--- Lab1/src/TestClass.java
+++ Lab1/src/TestClass.java
@@ -1,11 +1,10 @@
 
 /**
  * 
- * @author dmytro
- * Class that holds only public fields of uninitialized int & char with no methods to process them. 
+ * @author dmytro Class that holds only public fields of uninitialized int &
+ *         char with no methods to process them.
  */
-public class TestClass 
-{
+public class TestClass {
 
 	int intVar;
 	char charVar;
